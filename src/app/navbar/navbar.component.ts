c9a7368d3fb9395a3ecd7bafed0a1e91863a4881
Pankaj Phour <pankaj.phour70@gmail.com>
2023-02-27 18:16:12 +0530

    Basic design added

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+nav:any;
+burger:any;
+navOpen:boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.burger = document.getElementById('burger');
+     this.nav    = document.getElementById('main-nav');
+  }
+
+toggleNav(){
+  console.log("Hello from toggle nav");
+  setTimeout(() => {
+    
+    this.navOpen = !this.navOpen;
+  }, 100);
+  // this.classList.toggle('is-open');
+	this.nav.classList.toggle('is-open');
+}
+
+
+
+/* Onload demo - dirty timeout */
+
+}
+
+
