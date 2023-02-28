bc5ccfd12726e21850e8d502a7c2d33b66ec827d
Pankaj Phour <pankaj.phour70@gmail.com>
2023-02-28 16:29:50 +0530

    Design changed for classes section section and mobile UI updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  classes:any = [
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+    {name:'Weight Training',image:'/assets/gymB.webp',content:'Weight training is also known as resistance or strength training. It involves moving parts of your body against some kind of resistance, like weights, resistance bands, weight machines, or even your own body weight. Weight training is an excellent way to build muscle mass and make your muscles stronger.'},
+];
   constructor() { }
 
   ngOnInit(): void {
