a1837f1fcfd9b76477b9e8736680ee85ee200a65
Pankaj Phour <pankaj.phour70@gmail.com>
2023-03-03 18:01:24 +0530

    Body mass index calculator added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { MassCalculatorComponent } from './mass-calculator/mass-calculator.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
+  {path:'', component:MassCalculatorComponent},
+  {path:'home', component:HomeComponent},
   {path:'top', component:NavbarComponent},
   {path:'footer', component:FooterComponent},
 
